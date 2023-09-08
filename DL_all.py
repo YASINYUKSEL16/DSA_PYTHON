aec37d994f59ab52e59e4463a638194347e74727
Yasin Yüksel <yasinyksll99@gmail.com>
2023-09-08 21:12:36 +0300

    add miscellaneous scripts about LL and DL, organize these later

---
+++ DL_all.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+        
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
---
+++ LL_remove.py
@@ -0,0 +1,13 @@
+def remove(self, index):
+    if index < 0 or index >= self.length:
+        return None
+    if index == 0:
+        return self.pop_first()
+    if index == self.length:
+        return self.pop()
+    prev = self.get(index - 1)
+    temp = prev.next 
+    prev.next = temp.next
+    temp.next = None
+    self.length -= 1
+    return temp
